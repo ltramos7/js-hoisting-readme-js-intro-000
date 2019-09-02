27eb36f181f1e14806a4df2bea6530e08d95f29a
Linda T Ramos <ltramos7@gmail.com>
2019-09-02 21:48:21 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,19 +3,21 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+  function thisIsCrazy(){
     console.log("hey!!!")
   }
+
+  // var thisIsCrazy = function (){}
 }
+
+crazy();
 
 function sayMyName() {
   // fix the code in here:
@@ -24,7 +26,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+    
   }
 }
